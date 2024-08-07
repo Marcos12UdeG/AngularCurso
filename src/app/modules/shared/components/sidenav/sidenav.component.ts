101bf94915a2dd80d0e6a5c9ad1ff03b8776e05e
Marcos Isaac Acosta Garcia <marcosisaacacostagarcia@MacBook-Air-de-Marcos.local>
2024-08-07 13:00:32 -0600

    Cerrar sesion

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,15 +1,16 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit{
 
   mobileQuery: MediaQueryList;
-
+  username: any;
   menuNav = [
     {name: "Home",route: "home", icon:"home"},
     {name: "Categorias",route: "category", icon:"category"},
@@ -18,8 +19,17 @@ export class SidenavComponent {
 
   ]
 
+  private keyloak = inject(KeycloakService);
+
   constructor(media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  }
+  ngOnInit(): void {
+    this.username = this.keyloak.getUsername();
+  }
+
+  logout(){
+    this.keyloak.logout();
   }
 
 }
